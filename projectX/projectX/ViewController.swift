261fa57dc646d34b75b2d2ae8b818775ee82779a
rbezghin <radomirbezgin@gmail.com>
2020-06-13 14:31:18 -0700

    yay

---
+++ projectX/projectX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  projectX
+//
+//  Created by Radomyr Bezghin on 6/13/20.
+//  Copyright © 2020 Radomyr Bezghin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
