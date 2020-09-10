295ece3ade0a1d3777a5ebe743e39ff424eb58e2
rbezghin <radomirbezgin@gmail.com>
2020-09-10 14:36:58 -0700

    ui for individual post completed

---
+++ projectX/projectX/IndividualPost/AddNewCommentView.swift
@@ -0,0 +1,137 @@
+//
+//  NewCommentUIView.swift
+//  testing
+//
+//  Created by Radomyr Bezghin on 9/5/20.
+//  Copyright © 2020 Radomyr Bezghin. All rights reserved.
+//
+
+import UIKit
+
+class AddNewCommentView: UIView {
+    var commentTextViewHeightConstraint: NSLayoutConstraint? // used to make textview's height adjustable accoring to how much text there is
+    private let symbolsConfig = UIImage.SymbolConfiguration(pointSize: 20, weight: .light, scale: .default)
+    
+    override init(frame: CGRect) {
+        super.init(frame: CGRect(x: 0.0, y: 0.0, width: frame.width, height: 0.0))
+        setupViews()
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupViews()
+    }
+    let commentTextView: UITextView = {
+       let text = UITextView()
+       return text
+    }()
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "REPLY TICKET"
+        label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.numberOfLines = 1
+        return label
+    }()
+    private lazy var closeButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "xmark.circle")?.withConfiguration(symbolsConfig).withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
+        return button
+    }()
+    private lazy var authorView: UIImageView = {
+        let iv = UIImageView() 
+        iv.image = (UIImage(systemName: "person.circle")?.withConfiguration(symbolsConfig).withTintColor(.black, renderingMode: .alwaysOriginal))
+        return iv
+    }()
+    private let anonimousSwitch: UISwitch = {
+        let sw = UISwitch()
+        sw.transform = CGAffineTransform(scaleX: 0.75, y: 0.75);
+        return sw
+    }()
+    private lazy var attachButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "paperclip")?.withConfiguration(symbolsConfig).withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
+        return button
+    }()
+    private lazy var addImageButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "photo")?.withConfiguration(symbolsConfig).withTintColor(.black, renderingMode: .alwaysOriginal), for: .normal)
+        return button
+    }()
+    private let sendButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle(" SEND ", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.clipsToBounds = true
+        return button
+    }()
+    
+
+    
+    private func setupViews(){
+        self.backgroundColor = .white
+        self.layer.shadowColor = UIColor.black.cgColor
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowOffset = .zero
+        self.layer.shadowRadius = 5
+        [titleLabel,closeButton,commentTextView, authorView, anonimousSwitch, attachButton, addImageButton, sendButton].forEach {self.addSubview($0)}
+        self.addSubview(commentTextView)
+        
+        titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        titleLabel.addAnchors(top: self.topAnchor,
+                                 leading: nil,
+                                 bottom: nil,
+                                 trailing: nil,
+                                 padding: .init(top: Constants.standardPadding, left: 0, bottom: 0, right: 0))
+        
+        closeButton.addAnchors(top: self.topAnchor,
+                                 leading: self.leadingAnchor,
+                                 bottom: nil,
+                                 trailing: nil,
+                                 padding: .init(top: Constants.standardPadding, left: Constants.standardPadding, bottom: 0, right: 0))
+ 
+        commentTextView.addAnchors(top: closeButton.bottomAnchor,
+                                 leading: self.leadingAnchor,
+                                 bottom: anonimousSwitch.topAnchor,
+                                 trailing: self.trailingAnchor,
+                                 padding: .init(top: Constants.standardPadding, left: Constants.standardPadding, bottom: 0, right: Constants.standardPadding),
+                                 size: .init(width: 0, height: 0))
+
+        commentTextViewHeightConstraint = commentTextView.heightAnchor.constraint(equalToConstant: 0)
+        commentTextViewHeightConstraint?.isActive = true
+        
+        authorView.addAnchors(top: nil,
+                                 leading: self.leadingAnchor,
+                                 bottom: nil,
+                                 trailing: nil,
+                                 padding: .init(top: 0, left: Constants.standardPadding, bottom: 0, right: 0))
+        authorView.centerYAnchor.constraint(equalTo: anonimousSwitch.centerYAnchor).isActive = true
+        anonimousSwitch.addAnchors(top: nil,
+                                 leading: authorView.trailingAnchor,
+                                 bottom: self.bottomAnchor,
+                                 trailing: nil,
+                                 padding: .init(top: 0, left: Constants.standardPadding, bottom: Constants.standardPadding/2, right: 0))
+        
+        attachButton.addAnchors(top: nil,
+                                 leading: nil,
+                                 bottom: nil,
+                                 trailing: addImageButton.leadingAnchor,
+                                 padding: .init(top: 0, left: 0, bottom: 0, right: Constants.standardPadding),
+                                 size: .init(width: 0, height: anonimousSwitch.frame.height))
+        attachButton.centerYAnchor.constraint(equalTo: anonimousSwitch.centerYAnchor).isActive = true
+        addImageButton.addAnchors(top: nil,
+                                 leading: nil,
+                                 bottom: nil,
+                                 trailing: sendButton.leadingAnchor,
+                                 padding: .init(top: 0, left: 0, bottom: 0, right: Constants.standardPadding))
+        addImageButton.centerYAnchor.constraint(equalTo: anonimousSwitch.centerYAnchor).isActive = true
+        sendButton.addAnchors(top: nil,
+                                 leading: nil,
+                                 bottom: nil,
+                                 trailing: self.trailingAnchor,
+                                 padding: .init(top: 0, left: 0, bottom: 0, right: Constants.standardPadding))
+        sendButton.centerYAnchor.constraint(equalTo: anonimousSwitch.centerYAnchor).isActive = true
+        
+        
+    }
+}
---
+++ projectX/projectX/IndividualPost/CommentTableViewCell.swift
@@ -0,0 +1,167 @@
+//
+//  CommentTableViewCell.swift
+//  testing
+//
+//  Created by Radomyr Bezghin on 9/9/20.
+//  Copyright © 2020 Radomyr Bezghin. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+    
+    static let cellID = "CommentTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .white
+        backgroundColor = .clear
+        setupContentView()
+    }
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupContentView()
+    }
+    ///top stack
+    let extraTitleStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .fillProportionally
+        stack.spacing = 10
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+     let optionalAuthorExtraTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Deans Honor"
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        label.textAlignment = .left
+        label.textColor = .lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+     lazy var extraTitleImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = (UIImage(systemName: "star")?.withTintColor(.yellow, renderingMode: .alwaysOriginal))
+        return iv
+    }()
+    ///2nd stack
+    private let authorStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .fillProportionally
+        stack.spacing = 10
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    private lazy var circleBadgeView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = (UIImage(systemName: "circle")?.withTintColor(.lightGray, renderingMode: .alwaysOriginal))
+        return iv
+    }()
+    let authorTitleLable: UILabel = {
+        let label = UILabel()
+        label.text = "u/Sammy"
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        label.textColor = .lightGray
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    let dateTimeLabel: UILabel = {
+        let label = UILabel()
+        label.text = "1h"
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        label.textAlignment = .right
+        label.textColor = .lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    /// main comment area stack
+    private let commentStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.spacing = 10
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    let commentLabel: UILabel = {
+        let text = UILabel()
+        text.font = UIFont.preferredFont(forTextStyle: .callout)
+        text.numberOfLines = 0
+        text.adjustsFontSizeToFitWidth = false
+        text.lineBreakMode = .byTruncatingTail
+        text.text = "i am autoresizing text cell i am autoresizing text cell i am autoresizing text cell i am autoresizing text cell i am autoresizing text cell i am autoresizing text cell  "
+        text.textColor = .black
+        text.translatesAutoresizingMaskIntoConstraints = false
+        return text
+    }()
+    ///bottom stack
+    private let likesStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        stack.spacing = 10
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        return stack
+    }()
+    private let likeButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "chevron.up"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.translatesAutoresizingMaskIntoConstraints = false
+        //button.addTarget(self, action: #selector(likeButtonTouched), for: .touchUpInside)
+        return button
+    }()
+    private let likesLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0"
+        label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .subheadline)
+        label.numberOfLines = 1
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let dislikeButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "chevron.down"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.translatesAutoresizingMaskIntoConstraints = false
+        //button.addTarget(self, action: #selector(dislikeButtonTouched), for: .touchUpInside)
+        return button
+    }()
+    
+    private func  setupContentView(){
+        [optionalAuthorExtraTitle, extraTitleImageView].forEach({extraTitleStackView.addArrangedSubview($0)})
+        [authorTitleLable, dateTimeLabel].forEach { authorStackView.addArrangedSubview($0)}
+        [commentLabel].forEach { commentStackView.addArrangedSubview($0)}
+        [likeButton,likesLabel,dislikeButton].forEach { likesStackView.addArrangedSubview($0)}
+        [extraTitleStackView,authorStackView,commentStackView,likesStackView].forEach({self.contentView.addSubview($0)})
+        
+        extraTitleStackView.addAnchors(top: contentView.topAnchor,
+                                    leading: contentView.leadingAnchor,
+                                    bottom: nil,
+                                    trailing: nil,
+                                    padding: .init(top: Constants.standardPadding, left: Constants.standardPadding, bottom: 0, right: 0))
+        authorStackView.addAnchors(top: extraTitleStackView.bottomAnchor,
+                                    leading: contentView.leadingAnchor,
+                                    bottom: nil,
+                                    trailing: nil,
+                                    padding: .init(top: 0, left: Constants.standardPadding, bottom: 0, right: 0))
+        commentStackView.addAnchors(top: authorStackView.bottomAnchor,
+                                    leading: contentView.leadingAnchor,
+                                    bottom: nil,
+                                    trailing: contentView.trailingAnchor,
+                                    padding: .init(top: Constants.standardPadding, left: Constants.standardPadding, bottom: 0, right: Constants.standardPadding))
+        likesStackView.addAnchors(top: commentStackView.bottomAnchor,
+                                    leading: nil,
+                                    bottom: contentView.bottomAnchor,
+                                    trailing: nil,
+                                    padding: .init(top: Constants.standardPadding, left: 0, bottom: Constants.standardPadding, right: 0))
+        likesStackView.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+   
+    }
+
+}
