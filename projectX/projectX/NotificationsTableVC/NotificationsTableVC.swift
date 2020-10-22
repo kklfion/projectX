b97a873fe79800765a2e41a9da68b753b417ec98
Kirill Zhuze <megadymo@gmail.com>
2020-10-21 19:40:36 -0700

    basic read for missions/notifications page

--- projectX/projectX/NotificationsTableVC/NotificationsTableVC.swift
+++ projectX/projectX/NotificationsTableVC/NotificationsTableVC.swift
@@ -23,9 +23,9 @@ import UIKit
 class NotificationsTableVC: UIViewController {
     
     // make sure all lists have the same count
-    let notifications = ["UCSC's Channel will be undergoing a scheduled maintenance at 10pm PST. Click to learn more.","Hello fellow slugs, it's time to support our vets! Please join us in the Stevenson Lounge from 12pm to 2pm as we will be holding a fundraiser for our vets and host a ton of fun activities. See ya soon!","Freddy Mercury liked your post in the Gaming Station, check it out!","The wrestling season is starting soon! We're looking for big boy slugs, not skinny wimps. Click to learn more.", "Freddy Mercury replied to your post in the Gaming Station, check it out!"]
-    let notificationspic = ["sluglogoo.png", "slugvet.png", "xbox.png", "bigslug.png", "xbox.png"]
-    let notificationsdate = ["1h", "5h", "1d", "2y", "5y"]
+    var notifications = [String]()
+    var notificationspic = [String]()
+    var notificationsdate = [String]()
     
     
     
@@ -123,8 +123,25 @@ class NotificationsTableVC: UIViewController {
     var leftMissionAnchor : NSLayoutConstraint?
     var rightMissionAnchor : NSLayoutConstraint?
     
+    override func viewDidLayoutSubviews() {
+        GetData.getNotifications { (notifs) in
+            for notif in notifs {
+                self.notifications.append(notif.preview)
+                self.notificationsdate.append(notif.timestamp)
+                self.notificationspic.append(notif.image)
+            }
+            DispatchQueue.main.async {
+                self.notifTableView.reloadData()
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //NotificationsData.createNotifications()
+        
+        
         view.backgroundColor = .white
         
         notifTableView.delegate = self
@@ -132,6 +149,7 @@ class NotificationsTableVC: UIViewController {
         
         missionCollection.delegate = self
         missionCollection.dataSource = self
+        missionCollection.prefetchDataSource = self
         
         missionTable.delegate = self
         missionTable.dataSource = self
@@ -266,16 +284,17 @@ extension NotificationsTableVC: UITableViewDelegate, UITableViewDataSource {
             return cell
         }
         else {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: MissionsCell.Cell1ID, for: indexPath) as? MissionsCell else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MissionsCell.Cell1ID, for: indexPath) as? MissionsCell else {
                                 fatalError("Wrong cell")
                             }
             return cell
-                 }
-        
-}
+            
+        }
+    }
 
 
     func addData(toCell cell: NotificationCell, withIndex index: Int ){
+        
         cell.dateUILabel.text = notificationsdate[index]
         cell.previewtxtLabel.text = notifications[index]
         cell.notifimage.image = UIImage(named: notificationspic[index])
@@ -321,423 +340,8 @@ extension NotificationsTableVC: UICollectionViewDelegate, UICollectionViewDataSo
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-class NotificationsTableVC: UIViewController {
-    
-    let padding:CGFloat = 5
-
-    
-    let titleLabel : UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 28, weight: .bold)
-        label.text = "Mailroom"
-        label.textAlignment = .center
-        return label
-    }()
-    
-    let segmentController : UISegmentedControl = {
-        let sc = UISegmentedControl(items: ["Notifications", "Missions"])
-        sc.translatesAutoresizingMaskIntoConstraints = false
-        sc.selectedSegmentIndex = 0
-        sc.layer.backgroundColor = UIColor.white.cgColor
-        sc.addTarget(self, action: #selector(performAnimation), for: .valueChanged)
-        return sc
-    }()
-    
-    let viewForTableViews : UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    let notifTableView : UITableView = {
-        let tv = UITableView()
-        tv.translatesAutoresizingMaskIntoConstraints = false
-        tv.tableFooterView = UIView()
-        tv.register(NotificationCell.self, forCellReuseIdentifier: NotificationCell.CellID)
-        tv.rowHeight = UITableView.automaticDimension
-        tv.estimatedRowHeight = 100
-        return tv
-    }()
-    
-    let missionView : UIView = {
-        let view = UIView()
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
-    }()
-    
-    let missionTableHor : UITableView = {
-        let tv = UITableView()
-        tv.backgroundColor = .lightGray
-        //tv.transform = CGAffineTransform(rotationAngle: .pi / 2)
-        
-        tv.translatesAutoresizingMaskIntoConstraints = false
-        return tv
-    }()
-    
-    let missionTableVert : UITableView = {
-        let tv = UITableView()
-        tv.translatesAutoresizingMaskIntoConstraints = false
-        tv.tableFooterView = UIView()
-        tv.register(MissionsCell.self, forCellReuseIdentifier: MissionsCell.Cell1ID)
-        tv.rowHeight = UITableView.automaticDimension
-        tv.estimatedRowHeight = 100
-        return tv
-    }()
-    
-    // make sure all lists have the same count
-    let notifications = ["UCSC's Channel will be undergoing a scheduled maintenance at 10pm PST. Click to learn more.","Hello fellow slugs, it's time to support our vets! Please join us in the Stevenson Lounge from 12pm to 2pm as we will be holding a fundraiser for our vets and host a ton of fun activities. See ya soon!","Freddy Mercury liked your post in the Gaming Station, check it out!","The wrestling season is starting soon! We're looking for big boy slugs, not skinny wimps. Click to learn more.", "Freddy Mercury replied to your post in the Gaming Station, check it out!"]
-    let notificationspic = ["sluglogoo.png", "slugvet.png", "xbox.png", "bigslug.png", "xbox.png"]
-    let notificationsdate = ["1h", "5h", "1d", "2y", "5y"]
-    
-    
-    
-    
-    let missions = ["Need help fixing my laptop!", "Look after my cat", "Borrowing a mic!", "Grab some food for me", "Look after my pets!"]
-    let usernames = ["Brosef Doesef", "Kirill Zhuze", "Gurpreet Dhillon", "Jake Nations", "Radomyr Bezghin"]
-    // will make a uilabel for • in future
-    let missionsdate = ["• 7/30", "• 6/30", "• 7/30", "• 7/30", "• 7/30"]
-    let channels = ["Photography","Gaming","International","UCSC","UCSC"]
-    let missionactions = ["Selected","Completed","Completed","Completed","Completed"]
-    
-    
-    
-    var leftNotifAnchor : NSLayoutConstraint?
-    var rightNotifAnchor : NSLayoutConstraint?
-    var leftMissionAnchor : NSLayoutConstraint?
-    var rightMissionAnchor : NSLayoutConstraint?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        
-        notifTableView.delegate = self
-        notifTableView.dataSource = self
-        missionTableVert.delegate = self
-        missionTableVert.dataSource = self
-        
-        missionView.addSubview(missionTableHor)
-        missionView.addSubview(missionTableVert)
-        
-        viewForTableViews.addSubview(notifTableView)
-        viewForTableViews.addSubview(missionView)
-        
-        self.view.addSubview(titleLabel)
-        self.view.addSubview(segmentController)
-        self.view.addSubview(viewForTableViews)
-        
-        let titleLabelTop = titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 32)
-        let titleLabelLeft = titleLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 32)
-        let segmentControllerTop = segmentController.topAnchor.constraint(equalTo: titleLabel.bottomAnchor)
-        let segmentControllerWidth = segmentController.widthAnchor.constraint(equalTo: self.view.widthAnchor)
-        
-        NSLayoutConstraint.activate([titleLabelTop, titleLabelLeft,segmentControllerTop, segmentControllerWidth])
-        
-        // notification table constraints
-        
-        notifTableView.topAnchor.constraint(equalTo:
-            viewForTableViews.safeAreaLayoutGuide.topAnchor).isActive = true
-        notifTableView.bottomAnchor.constraint(equalTo:
-            viewForTableViews.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        
-        leftNotifAnchor = notifTableView.leadingAnchor.constraint(equalTo: viewForTableViews.safeAreaLayoutGuide.leadingAnchor)
-        leftNotifAnchor?.isActive = true
-        rightNotifAnchor = notifTableView.trailingAnchor.constraint(equalTo: viewForTableViews.safeAreaLayoutGuide.leadingAnchor)
-        rightNotifAnchor?.isActive = false
-        
-        notifTableView.widthAnchor.constraint(equalTo:
-            viewForTableViews.safeAreaLayoutGuide.widthAnchor).isActive = true
-        
-        // mission view constraints
-        
-        missionView.topAnchor.constraint(equalTo:
-            viewForTableViews.safeAreaLayoutGuide.topAnchor).isActive = true
-        missionView.bottomAnchor.constraint(equalTo:
-            viewForTableViews.safeAreaLayoutGuide.bottomAnchor).isActive = true
-        
-        leftMissionAnchor = missionView.leadingAnchor.constraint(equalTo: viewForTableViews.safeAreaLayoutGuide.trailingAnchor)
-        leftMissionAnchor?.isActive = true
-        rightMissionAnchor = missionView.trailingAnchor.constraint(equalTo: viewForTableViews.safeAreaLayoutGuide.trailingAnchor)
-        rightMissionAnchor?.isActive = false
-        
-        missionView.widthAnchor.constraint(equalTo:
-            viewForTableViews.safeAreaLayoutGuide.widthAnchor).isActive = true
-        
-        // horizontal mission table constraints
-        
-        missionTableHor.addAnchors(
-            top: missionView.safeAreaLayoutGuide.topAnchor,
-            leading: missionView.safeAreaLayoutGuide.leadingAnchor,
-            bottom: missionView.safeAreaLayoutGuide.centerYAnchor,
-            trailing: missionView.safeAreaLayoutGuide.trailingAnchor,
-            padding: .init(top: padding + 30, left: 0, bottom: 0, right: 0)
-            
-        
-        )
-        
-        // vertical mission table constraints
-        
-        missionTableVert.addAnchors(
-            top: missionView.safeAreaLayoutGuide.centerYAnchor,
-            leading: missionView.safeAreaLayoutGuide.leadingAnchor,
-            bottom: missionView.safeAreaLayoutGuide.bottomAnchor,
-            trailing: missionView.safeAreaLayoutGuide.trailingAnchor)
-        
-        // view for other views constraints
-        
-        viewForTableViews.addAnchors(top: segmentController.bottomAnchor, leading: self.view.leadingAnchor, bottom: self.view.bottomAnchor, trailing: self.view.trailingAnchor)
-    }
-   
-    
-    @objc func performAnimation(){
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveEaseOut,
-            animations:
-            {
-                if(self.segmentController.selectedSegmentIndex == 0){
-                    //move home to the right
-                    self.leftNotifAnchor?.isActive = true
-                    self.rightNotifAnchor?.isActive = false
-                    //move recommendation to the right
-                    self.leftMissionAnchor?.isActive = true
-                    self.rightMissionAnchor?.isActive = false
-                    self.viewForTableViews.layoutIfNeeded()
-                } else if (self.segmentController.selectedSegmentIndex == 1){
-                    //move home to the left
-                    self.leftNotifAnchor?.isActive = false
-                    self.rightNotifAnchor?.isActive = true
-                    //move recommendation to the left
-                    self.leftMissionAnchor?.isActive = false
-                    self.rightMissionAnchor?.isActive = true
-                    self.viewForTableViews.layoutIfNeeded()
-                }
-        }, completion: nil)
-    }
-    
-    
-}
-
-extension NotificationsTableVC: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notifications.count
-    }
- 
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if tableView == self.notifTableView {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: NotificationCell.CellID, for: indexPath) as? NotificationCell else {
-                   fatalError("Wrong cell")
-               }
-        addData(toCell: cell, withIndex: indexPath.row)
-        return cell
-        }
-        else if tableView == self.missionTableVert{
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: MissionsCell.Cell1ID, for: indexPath) as? MissionsCell else {
-                       fatalError("Wrong cell")
-                   }
-            addData1(toCell: cell, withIndex: indexPath.row)
-            return cell
-        }
-        else {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: MissionsCell.Cell1ID, for: indexPath) as? MissionsCell else {
-                                fatalError("Wrong cell")
-                            }
-            return cell
-                 }
-        
-}
-
-
-    func addData(toCell cell: NotificationCell, withIndex index: Int ){
-        cell.dateUILabel.text = notificationsdate[index]
-        cell.previewtxtLabel.text = notifications[index]
-        cell.notifimage.image = UIImage(named: notificationspic[index])
-    }
-    func addData1(toCell cell: MissionsCell, withIndex index: Int ){
-        cell.dateUILabel.text = missionsdate[index]
-        cell.nameUILabel.text = usernames[index]
-        cell.previewtxtLabel.text = missions[index]
-        cell.selectionUILabel.text = missionactions[index]
-        cell.channelUILabel.setTitle(channels[index], for: .normal)
-        //cell.channelUILabel.text = channels[index]
-        cell.notifimage.image = UIImage(named: notificationspic[index])
+extension NotificationsTableVC: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        print("Prefetch: \(indexPaths)")
     }
 }
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-import UIKit
-
-class NotificationsTableVC: UIViewController {
-    
-    
-    let titleLabel: UILabel = {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 28, weight: .bold)
-        label.text = "Mailroom"
-        label.textAlignment = .center
-        return label
-    }()
-    
-    let segmentController: UISegmentedControl = {
-        let sc = UISegmentedControl(items: ["Notifications", "Missions"])
-        sc.translatesAutoresizingMaskIntoConstraints = false
-        sc.selectedSegmentIndex = 0
-        sc.layer.backgroundColor = UIColor.white.cgColor
-        sc.addTarget(self, action: #selector(performAnimation), for: .valueChanged)
-        return sc
-    }()
-    
-    
-    // should we add another label to preview the actually reply of the person?
-    
-    
-    // make sure all lists have the same count
-    let notifications = ["UCSC's Channel will be undergoing a scheduled maintenance at 10pm PST. Click to learn more.","Hello fellow slugs, it's time to support our vets! Please join us in the Stevenson Lounge from 12pm to 2pm as we will be holding a fundraiser for our vets and host a ton of fun activities. See ya soon!","Freddy Mercury liked your post in the Gaming Station, check it out!","The wrestling season is starting soon! We're looking for big boy slugs, not skinny wimps. Click to learn more.", "Freddy Mercury replied to your post in the Gaming Station, check it out!"]
-    let notificationspic = ["sluglogoo.png", "slugvet.png", "xbox.png", "bigslug.png", "xbox.png"]
-    let notificationsdate = ["1h", "5h", "1d", "2y", "5y"]
-    let tableView = UITableView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        tableView.tableFooterView = UIView()
-      
-        tableView.delegate = self
-        tableView.dataSource = self
-      
-        tableView.register(NotificationCell.self, forCellReuseIdentifier: NotificationCell.CellID)
-        tableView.rowHeight = UITableView.automaticDimension
-       
-        tableView.estimatedRowHeight = 100
-        self.view.addSubview(titleLabel)
-        self.view.addSubview(segmentController)
-        self.view.addSubview(tableView)
-        
-        let titleLabelTop = titleLabel.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 32)
-        let titleLabelLeft = titleLabel.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 32)
-        let segmentControllerTop = segmentController.topAnchor.constraint(equalTo: titleLabel.bottomAnchor)
-        let segmentControllerWidth = segmentController.widthAnchor.constraint(equalTo: self.view.widthAnchor)
-        
-        tableView.addAnchors(top: segmentController.bottomAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
-
-        NSLayoutConstraint.activate([titleLabelTop, titleLabelLeft,segmentControllerTop, segmentControllerWidth])
-        
-    }
-   
-    
-    @objc func performAnimation(){
-        
-    }
-    
-    
-}
-
-extension NotificationsTableVC: UITableViewDelegate, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return notifications.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: NotificationCell.CellID, for: indexPath) as? NotificationCell else {
-                   fatalError("Wrong cell")
-               }
-
-               addData1(toCell: cell, withIndex: indexPath.row)
-               return cell
-        
-        
-        
-        
-        
-        return UITableViewCell()
-        
-        
-      
-    }
-
-    func addData1(toCell cell: NotificationCell, withIndex index: Int ){
-        cell.dateUILabel.text = notificationsdate[index]
-        cell.previewtxtLabel.text = notifications[index]
-        cell.notifimage.image = UIImage(named: notificationspic[index])
-
-    }
-    
-
-}
-
-
- */
