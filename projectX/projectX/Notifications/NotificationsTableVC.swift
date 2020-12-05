f24dc939d7401d6469b5677dcb27d590cd3caede
Radomyr Bezghin <radomirbezgin@gmail.com>
2020-12-04 20:14:25 -0800

    final touches

--- projectX/projectX/Notifications/NotificationsTableVC.swift
+++ projectX/projectX/Notifications/NotificationsTableVC.swift
@@ -37,10 +37,22 @@ class NotificationsTableVC: UICollectionViewController {
         collectionView.backgroundColor = .white
         navigationController?.navigationBar.prefersLargeTitles = true
         self.navigationItem.title = "Mailroom"
-        navigationItem.searchController = UISearchController(searchResultsController: nil)
+        let search = UISearchController(searchResultsController: nil)
+        navigationItem.searchController = search
         
         collectionView.register(NotificationCell.self, forCellWithReuseIdentifier: NotificationCell.CellID)
         getData()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithOpaqueBackground()
+        navBarAppearance.backgroundColor = Constants.yellowColor
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
+        navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.navigationBar.prefersLargeTitles = false
     }
     private func getData(){
         let query = NetworkManager.shared.db.notifications
@@ -80,86 +92,5 @@ extension NotificationsTableVC: UICollectionViewDelegateFlowLayout{
             cell.textLabel.text = notifications[index].preview
             cell.notificationImageView.image = UIImage(named: notifications[index].image)
         }
-    
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return notifications.count
-//    }
-//
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if tableView == self.notifTableView {
-//        guard let cell = tableView.dequeueReusableCell(withIdentifier: NotificationCell.CellID, for: indexPath) as? NotificationCell else {
-//                   fatalError("Wrong cell")
-//               }
-//        addData(toCell: cell, withIndex: indexPath.row)
-//        return cell
-//        }
-//        else if tableView == self.missionTable{
-//            guard let cell = tableView.dequeueReusableCell(withIdentifier: MissionsCell.Cell1ID, for: indexPath) as? MissionsCell else {
-//                       fatalError("Wrong cell")
-//                   }
-//            addData1(toCell: cell, withIndex: indexPath.row)
-//            return cell
-//        }
-//        else {
-//            guard let cell = tableView.dequeueReusableCell(withIdentifier: MissionsCell.Cell1ID, for: indexPath) as? MissionsCell else {
-//                                fatalError("Wrong cell")
-//                            }
-//            return cell
-//
-//        }
-//    }
-//
-//
-//    func addData(toCell cell: NotificationCell, withIndex index: Int ){
-//
-//        cell.dateUILabel.text = notificationsdate[index]
-//        cell.previewtxtLabel.text = notifications[index]
-//        cell.notifimage.image = UIImage(named: notificationspic[index])
-//    }
-//    func addData1(toCell cell: MissionsCell, withIndex index: Int ){
-//        cell.dateUILabel.text = missions1date[index]
-//        cell.nameUILabel.text = usernames[index]
-//        cell.previewtxtLabel.text = missions1[index]
-//        cell.selectionUILabel.text = missionactions[index]
-//        cell.channelUILabel.setTitle(channels[index], for: .normal)
-//        //cell.channelUILabel.text = channels[index]
-//        cell.notifimage.image = UIImage(named: notificationspic[index])
-//    }
 }
 
-//extension NotificationsTableVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        return missions.count
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//
-//
-//
-//        guard let cell = missionCollection.dequeueReusableCell(
-//            withReuseIdentifier: MissionCell.CellID, for: indexPath) as? MissionCell else {fatalError("Wrong cell") }
-//
-//        addDataMission(toCell: cell, withIndex: indexPath.item)
-//        return cell
-//    }
-//
-//    func addDataMission(toCell cell: MissionCell, withIndex index: Int) {
-//        cell.deadlineLabel.text = "In \(missionsdate[index])"
-//        cell.previewtxtLabel.text = "\"\(missions[index])\""
-//        cell.missionImage.image = UIImage(named: missionspic[index])
-//    }
-//
-//    func collectionView(_ collectionView: UICollectionView,
-//                        layout collectionViewLayout: UICollectionViewLayout,
-//                        sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: collectionView.bounds.height - 10, height: collectionView.bounds.height - 10)
-//    }
-//}
-//
-//
-//extension NotificationsTableVC: UICollectionViewDataSourcePrefetching {
-//    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
-//        print("Prefetch: \(indexPaths)")
-//    }
-//}
