7e1572bdef18c15ba1cabd83d083f932d24dbc8e
Gurpreet Dhillon <gudhillon@gmail.com>
2021-01-26 15:20:46 -0800

    privacy policy & contact us setup

---
+++ projectX/projectX/Sidebar/Settings/ContactUsViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ContactUsViewController.swift
+//  projectX
+//
+//  Created by Gurpreet Dhillon on 1/26/21.
+//  Copyright © 2021 Radomyr Bezghin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ContactUsViewController: UIViewController {
+    
+    lazy var CUView = createContactUsView()
+    
+    lazy var contentView : UIView = {
+            let view = UIView()
+            view.translatesAutoresizingMaskIntoConstraints = false
+            return view
+        }()
+    
+    lazy var scrollView : UIScrollView = {
+         let scrollView = UIScrollView(frame: .zero)
+         scrollView.backgroundColor = .white
+         scrollView.bounces = true
+         scrollView.translatesAutoresizingMaskIntoConstraints = false
+         return scrollView
+     }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        self.navigationController?.navigationBar.isHidden = true
+        setupView()
+        
+    }
+    func createContactUsView()-> ContactUsView{
+        let view = ContactUsView(frame: self.view.frame)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.cancelButton.addTarget(self, action: #selector(dismissAction), for: .touchUpInside)
+        return view
+    }
+    
+    
+    ///scrollView<-contentView<-TOSView
+    func setupView(){
+        /*view.addSubview(scrollView)
+        self.addKeyboardTapOutGesture(target: self)
+        scrollView.addAnchors(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+      
+        scrollView.addSubview(contentView)
+        contentView.addAnchors(top: scrollView.topAnchor, leading: scrollView.leadingAnchor, bottom: scrollView.bottomAnchor, trailing: scrollView.trailingAnchor)
+        contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
+        
+        contentView.addSubview(CUView)
+        CUView.addAnchors(top: contentView.topAnchor, leading: contentView.leadingAnchor, bottom: contentView.bottomAnchor, trailing: contentView.trailingAnchor)*/
+        self.view.addSubview(CUView)
+        CUView.addAnchors(top: view.topAnchor, leading: view.leadingAnchor, bottom: view.bottomAnchor, trailing: view.trailingAnchor)
+        
+    }
+    
+    @objc func dismissAction(_ sender:UIButton!) {
+        dismiss(animated: true, completion: nil)
+    }
+}
